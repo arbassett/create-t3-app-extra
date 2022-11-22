a4b8b04845eac40edb0e9ac14623430305a1f72f
Aaron Bassett <arbassett4@outlook.com>
2022-11-22 15:23:21 -0500

    fix(vite:base): remove env check on build

--- packages/cli/templates/vite:base/vite.config.ts
+++ packages/cli/templates/vite:base/vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/client.mjs"));
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
